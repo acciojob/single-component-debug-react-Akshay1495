2e4e3ed8fbda893cc0256b9f863b325cffd86ff2
Akshay <8009akshy@gmail.com>
2024-07-17 08:38:32 +0530

    solved

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+let container = document.getElementById('root')
+ReactDOM.render(<App />, container);
+
+
 
 
